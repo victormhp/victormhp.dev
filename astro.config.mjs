2187aa7f01f9e0dbc271ae3e81d9bea819ce63a3
victormhp <victoralbertomhp@gmail.com>
2025-02-04 13:34:06 -0600

    Improve SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    inlineStylesheets: "always",
+  },
+  compressHTML: true,
+  prefetch: true,
   vite: { plugins: [tailwindcss()] },
+  integrations: [sitemap()],
   devToolbar: {
     enabled: false,
   },
